07e76cc0c72832cfec1c0951e8c913ae31505f9f
sharadkale2611 <sharadkale2611@gmail.com>
2026-01-01 19:16:51 +0530

    dashboard cleared

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,11 +1,4 @@
 import type { Metadata } from "next";
-import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
-import React from "react";
-import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
-import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "@/components/ecommerce/StatisticsChart";
-import RecentOrders from "@/components/ecommerce/RecentOrders";
-import DemographicCard from "@/components/ecommerce/DemographicCard";
 
 export const metadata: Metadata = {
   title:
@@ -17,9 +10,10 @@ export default function Ecommerce() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
       <div className="col-span-12 space-y-6 xl:col-span-12">
-        <EcommerceMetrics />
+        Coming soon...
+        {/* <EcommerceMetrics /> */}
 
-        <MonthlySalesChart />
+        {/* <MonthlySalesChart /> */}
       </div>
 
       {/* <div className="col-span-12 xl:col-span-5">
@@ -35,7 +29,7 @@ export default function Ecommerce() {
       </div> */}
 
       <div className="col-span-12 xl:col-span-12">
-        <RecentOrders />
+        {/* <RecentOrders /> */}
       </div>
     </div>
   );
